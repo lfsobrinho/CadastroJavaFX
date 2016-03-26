9562a48c6c62939446dc7fb8ffe5bad32e8ef648
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-03-26 17:58:18 -0300

    Adicionando o css e o icone

--- TrabalhoJavaFx/src/ch/makery/address/MainApp.java
+++ TrabalhoJavaFx/src/ch/makery/address/MainApp.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -52,7 +53,10 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.setTitle("CadastroApp");
+        
+        this.primaryStage.getIcons().add(new Image("file:resources/images/1459042757_Address_Book.png"));
+
 
         initRootLayout();
 
